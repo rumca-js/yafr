21a832aa6cc01a63be4705650f79279eda9bab35
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2026-03-26 16:22:11 +0100

    Background jobs work

---
+++ src/entryrules.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from datetime import datetime
+from .sourcedata import SourceData
+from .sources import Sources
+
+
+def read_line_things(input_text):
+    sources = [
+        line.strip()
+        for line in input_text.splitlines()
+        if line.strip()
+    ]
+
+    sources = set(sources)
+    sources = list(sources)
+
+    return sources
+
+
+class EntryRules(object):
+    def __init__(self, connection):
+        self.connection = connection
+
+    def is_entry_rule_triggered(self, url) -> bool:
+        rules = self.connection.entry_rules.get_where({"trigger_rule_url" : url})
+        rules = next(rules, None)
+        if rules:
+            return True
+        return False
+
+    def add_entry_rules(self, raw_input):
+        self.connection.entry_rules.truncate()
+
+        entry_rule_urls = read_line_things(raw_input)
+        for entry_rule_url in entry_rule_urls:
+            self.add_entry_rule(entry_rule_url)
+
+    def get_rule_urls(self):
+        urls = []
+
+        rules = self.connection.entry_rules.get_where(limit=10000)
+        for rule in rules:
+            if not rule.enabled:
+                continue
+
+            urls.append(rule.trigger_rule_url)
+
+        return urls
+
+    def get_rules_for(self, url=None, entry=None):
+        result = []
+
+        rules = self.connection.entry_rules.get_where(limit=10000)
+        for rule in rules:
+           if not rule.enabled:
+               continue
+
+           if self.is_entry_rule_triggered(entry["link"]):
+               result.append(rule)
+
+        return result
+
+    def add_entry_rule(self, entry_rule):
+        entries = self.connection.entry_rules.get_where({"trigger_rule_url" : entry_rule})
+        entry = next(entries, None)
+
+        if not entry:
+            data = {}
+            data["trigger_rule_url"] = entry_rule
+            data["enabled"] = True
+            data["priority"] = 0
+            data["rule_name"] = entry_rule
+            data["trigger_text"] = ""
+            data["trigger_text_hits"] = 0
+            data["trigger_text_fields"] = ""
+            data["block"] = True
+            data["trust"] = False
+            data["auto_tag"] = ""
+            data["apply_age_limit"] = 0
+            data["browser_id"] = 0
+
+            self.connection.entry_rules.insert_json_data(data)
+
+    def truncate(self):
+        self.connection.entry_rules.truncate()
+
+    def close(self):
+        if self.connection:
+            self.connection.close()
+            self.connection = None
