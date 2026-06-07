75bd00958ec1a58d528b01daaf87eca712dbc678
Rumpel Stilskin <rozbujnik@gmial.com>
2026-06-07 23:08:51 +0200

    Fixed searchview endpoints

--- main.py
+++ main.py
@@ -992,12 +992,15 @@ def view_add():
 
     if request.method == "POST":
         views = SearchView(connection = connection)
+        view_id = views.add()
+        view = views.get(id=view_id)
 
         html_text = get_view(VIEW_ADD_TEMPLATE, title="View add")
         connection.close()
         return render_template_string(html_text, view=view)
 
     html_text = get_view(VIEW_ADD_TEMPLATE, title="View add")
+    connection.close()
     return render_template_string(html_text)
 
 
@@ -1011,6 +1014,7 @@ def view_edit():
     view = views.get(id=view_id)
 
     if request.method == "POST":
+        json_data = {}
         json_data["name"] = request.args.get("Name")
         json_data["default"] = request.args.get("default")
         json_data["priority"] = request.args.get("priority")
@@ -1018,14 +1022,15 @@ def view_edit():
         json_data["order_by"] = request.args.get("order_by")
 
         json_data["default"] = json_data["default"] == "True"
-        
-        connection.searchviews.update_json_data(id = view_id, json_data=json_data)
+
+        connection.searchview.update_json_data(id = view_id, json_data=json_data)
 
         connection.close()
-        html_text = get_view(VIEW_EDIT_TEMPLATE, title="View edit")
+        html_text = get_view(VIEW_ADD_TEMPLATE, title="View edit")
         return render_template_string(html_text, view=view)
 
     html_text = get_view(VIEW_ADD_TEMPLATE, title="View edit")
+    connection.close()
     return render_template_string(html_text, view=view)
 
 
@@ -1036,12 +1041,11 @@ def view_remove():
 
     view_id = request.args.get("id")
 
-    view = SearchViews(connection = connection)
-    view.delete(id=view_id)
+    connection.searchview.delete(id=view_id)
 
     html_text = get_view(OK_TEMPLATE, title="Remove view")
+    connection.close()
     return render_template_string(html_text)
-
 
 @app.route("/remove-all-entries")
 def remove_all_entries():
@@ -1145,7 +1149,7 @@ def remove_all_entry_rules():
 @app.route("/remove-all-views")
 def remove_all_views():
     connection = DbConnection(app.config["DB_FILE"])
-    connection.searchviews.truncate()
+    connection.searchview.truncate()
     connection.close()
 
     html_text = get_view(OK_TEMPLATE, title="Remove Search views OK")
---
+++ tests/test_main_views.py
@@ -0,0 +1,63 @@
+from tests.dbtestcase import DbTestCase
+from linkarchivetools.model import SearchView
+from main import app
+
+class MainViewsTest(DbTestCase):
+    def add_view(self):
+        json_data = {}
+        json_data["name"] = "Test View"
+        json_data["default"] = True
+        json_data["priority"] = 1
+        json_data["filter_statement"] = ""
+        json_data["order_by"] = ""
+
+        controller = SearchView(connection=self.connection)
+        view_id = controller.add()
+        return view_id
+
+    def test_view_edit_get(self):
+        self.initialize_database()
+        view_id = self.add_view()
+        self.connection.close()
+
+        client = app.test_client()
+        response = client.get(f"/view-edit?id={view_id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_edit_post(self):
+        self.initialize_database()
+        view_id = self.add_view()
+        self.connection.close()
+
+        client = app.test_client()
+        # The code uses request.args.get even for POST, which is weird, but let's test it as is first
+        response = client.post(f"/view-edit?id={view_id}&Name=Updated+View&default=False&priority=2&filter_statement=test&order_by=id")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_add_post(self):
+        self.initialize_database()
+        self.connection.close()
+
+        client = app.test_client()
+        response = client.post("/view-add")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_remove(self):
+        self.initialize_database()
+        view_id = self.add_view()
+        self.connection.close()
+
+        client = app.test_client()
+        response = client.get(f"/view-remove?id={view_id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Remove view", response.data)
+
+    def test_remove_all_views(self):
+        self.initialize_database()
+        self.add_view()
+        self.connection.close()
+
+        client = app.test_client()
+        response = client.get("/remove-all-views")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Remove Search views OK", response.data)
